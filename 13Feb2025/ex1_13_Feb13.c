70305ad5e3116d513af46305ee55804f3e74b1de
girinith <girinithrs@gmail.com>
2025-02-16 10:14:42 +0530

    initial commit

--- 13Feb2025/ex1_13_Feb13.c
+++ 13Feb2025/ex1_13_Feb13.c
@@ -1,101 +1,38 @@
-/*Exercise 1-22. Write a program to ``fold'' long input lines into two or more shorter lines after
- the last non-blank character that occurs before the n-th column of input. Make sure your
- program does something intelligent with very long lines, and if there are no blanks or tabs
- before the specified column.
-*/
 #include <stdio.h>
-
-#define MAXLINE 10000
-
-#define TRUE 1
-#define FALSE 0
-
-#define BREAKING_POINT 20
-#define OFFSET 5
-
-int get_line(char line[], int max_line_len);
-void fold_line(char line[], char fold_str[], int n_break);
-
-int main(void)
+//program to print the input one word per inline
+#define IN 1
+#define OUT 0
+int main()
 {
-  char line[MAXLINE];
-  char fold_str[MAXLINE];
+    int c,state,wc,lc=0;
+    int wl[10];
+    state = OUT;
+    int max = 0;
+    wc = 0;
+    while ((c = getchar()) != EOF){
 
-  while ((get_line(line, MAXLINE)) > 0)
-  {
-    fold_line(line, fold_str, BREAKING_POINT);
-    printf("%s", fold_str);
-  }
-
-  return 0;
-}
-
-int get_line(char line[], int max_line_len)
-{
-  int c, i = 0;
-
-  while (i < max_line_len - 1 && (c = getchar()) != EOF && c != '\n')
-  {
-    line[i++] = c;
-  }
-
-  if (c == '\n')
-  {
-    line[i++] = c;
-  }
-
-  line[i] = '\0';
-
-  return i;
-}
-
-void fold_line(char line[], char fold_str[], int n_break)
-{
-  int i, j;
-  int column = 0;
-  int split = FALSE;
-  int last_blank = 0;
-
-  for (i = 0, j = 0; line[i] != '\0'; ++i, ++j)
-  {
-    fold_str[j] = line[i];
-
-    if (fold_str[j] == '\n')
-    {
-      column = 0;
+        if (c != ' ' && c != '\n' && c != '\t')
+            state = IN;
+        else{
+            state = OUT;
+            wl[wc++] = lc;
+            max = max>lc ? max : lc ;
+            lc =0;
+        }
+        if (state == IN && c != ' ' && c != '\n')
+            ++lc;
+        if (c == '.') break;//to stop getting input after encountering '.'
     }
 
-    column++;
+    while(max){
+        //this double loop is to print the histogram
+        for (int i=0;i<wc;i++){
+            if (wl[i] >= max) printf("* ");
+            else printf("  ");
+        }
+        printf("\n");
+        --max;
+        }
 
-    if (column == n_break - OFFSET)
-    {
-      split = TRUE;
-    }
-
-    if (split && (fold_str[j] == ' ' || fold_str[j] == '\t'))
-    {
-      last_blank = j;
-    }
-
-    if (column == n_break)
-    {
-      if (last_blank)
-      {
-        fold_str[last_blank] = '\n';
-        column = j - last_blank;
-        last_blank = 0;
-      }
-      else
-      {
-        fold_str[j++] = '-';
-        fold_str[j] = '\n';
-
-        column = 0;
-      }
-
-      split = FALSE;
-    }
-  }
-
-  fold_str[j] = '\0';
+    return 0;
 }
